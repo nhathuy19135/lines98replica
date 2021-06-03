401ef5010be36e846af61302cf3b0ed69e03b9be
nhathuy19135 <74639606+nhathuy19135@users.noreply.github.com>
2021-06-03 11:02:49 +0700

    added images + gifs

---
+++ PlayGame.java
@@ -0,0 +1,11 @@
+import Lines.*;
+
+public class PlayGame{
+	
+	public static void main(String[] args)throws Exception{	
+				
+        LineFrame app = new LineFrame();
+        app.startGame();           
+         	
+	}
+}
