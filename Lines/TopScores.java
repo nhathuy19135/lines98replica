bb5ab95c3f7132ad6a50cdd20a1863e118910b81
namnguyen3920 <65067380+namnguyen3920@users.noreply.github.com>
2021-06-07 10:33:50 +0700

    Add files via upload

---
+++ Lines/TopScores.java
@@ -0,0 +1,85 @@
+package Lines;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.io.*;
+
+public class TopScores extends JFrame{
+	
+	public Player  player[] =new Player[10];
+	public JButton  number []=new JButton[10];
+	
+	public TopScores(){
+		
+		for(int i=0;i<10;i++){
+			this.player[i]=new Player();
+			this.player[i].name= "player"+(i+1);
+		}
+		for(int i=0;i<10;i++){
+			number[i]=new JButton((i+1)+". "+this.player[i].name+"  :   "+this.player[i].scores);
+			add(number[i]);
+		}
+		setTitle(" TopScores");
+		setSize(200,320);
+		setLayout(new GridLayout(10,1));
+		setResizable(true);		
+	}
+	
+	//Sort player name
+	public void sortPlayer(){
+		for(int i=0;i<10;i++)
+			for(int j=i;j>0;j--)
+				if(player[j].scores>player[j-1].scores){
+					Player tmp = this.player[j];
+					this.player[j]  = this.player[j-1];
+					this.player[j-1]= tmp;
+				}		
+	}
+    
+    //Add 1 player to TopScores
+	public void add(Player newPlayer)throws IOException{
+		readFile();
+		if(this.player[9].scores<newPlayer.scores){
+		    this.player[9] = newPlayer;
+			sortPlayer();
+			for(int i=0;i<10;i++){
+				this.number[i].setText((i+1)+". "+player[i].name+"  :   "+player[i].scores);
+			}
+			writeFile();//Export to file TopScores.dat
+		}
+	}
+	
+	//Write player info to TopScores.dat
+	public void writeFile()throws IOException {
+	 	DataOutputStream out = new DataOutputStream(new FileOutputStream("TopScores.dat"));
+		for(int i=0;i<10;i++)
+			this.player[i].writeData(out);
+        out.close();
+	}
+	
+	//Read player info from TopScores.dat
+	public void readFile()throws IOException {	
+		RandomAccessFile in = new RandomAccessFile("TopScores.dat","r");
+		for(int i=0;i<10;i++)
+			this.player[i].readData(in);
+		sortPlayer();
+		for(int i=0;i<10;i++){
+			this.number[i].setText((i+1)+". "+player[i].name+"  :   "+player[i].scores);
+		}
+	}
+	
+	//Display TopScores
+	public void showTopScores()throws IOException{
+		readFile();
+		show();
+	}
+	
+	//reset TopScores
+	public void resetTopScores()throws IOException{
+		new TopScores();
+		writeFile();
+	}	
+	
+
+}
